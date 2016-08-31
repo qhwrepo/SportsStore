6d335cdd2695abcf430102cb03fb044036cd7cdf
Qinghang Wu <Qinghang Wu>
2016-09-01 09:50:47 +1000

    Adding pagination

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,21 +5,27 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
 
         }
         // GET: Product
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repository.Products.OrderBy(p=>p.ProductID).Skip((page - 1) * PageSize).Take(PageSize), PagingInfo = new PagingInfo { CurrentPage = page, ItemsPerPage = PageSize, TotalItems = repository.Products.Count()}
+            };
+            return View(model);
         }
     }
 }
